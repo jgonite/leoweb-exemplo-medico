914db70c11d669121c52289b80865202dec93145
LEONARDO ALMEIDA DINIZ <jgonite>
2025-05-13 12:41:47 -0300

    ecommerce mobile

--- src/app/exemplo-ecommerce/exemplo-ecommerce.component.ts
+++ src/app/exemplo-ecommerce/exemplo-ecommerce.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exemplo-ecommerce',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exemplo-ecommerce.component.html',
   styleUrl: './exemplo-ecommerce.component.css'
 })
@@ -13,4 +14,36 @@ export class ExemploEcommerceComponent {
   imgSrc3 = 'assets/eg3r.png';
   imgSrc4 = 'assets/eg4r.png';
   imgSrc5 = 'assets/eg5r.png';
+
+  isMobileMenuOpen = false;
+  isSubMenuOpen = {
+    roupas: false,
+    calcados: false
+  };
+  
+  
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    if (!this.isMobileMenuOpen) {
+      this.closeAllSubmenus();
+    }
+  }
+  
+  closeAllSubmenus() {
+    const keys = Object.keys(this.isSubMenuOpen) as (keyof typeof this.isSubMenuOpen)[];
+    for (const key of keys) {
+      this.isSubMenuOpen[key] = false;
+    }
+  }
+  
+  
+  openSubMenu(menu: 'roupas' | 'calcados') {
+    this.closeAllSubmenus();
+    this.isSubMenuOpen[menu] = true;
+  }
+  
+  closeSubMenu(menu: 'roupas' | 'calcados') {
+    this.isSubMenuOpen[menu] = false;
+  }
+
 }
